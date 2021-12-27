ea12aa525f7a9656af1296172c8ddf3dd5077091
Deepak Putta <Deepakputta.c@gmail.com>
2021-12-27 07:57:19 -0700

    Added RDS instance

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -9,3 +9,11 @@ variable "project" {
 variable "contact" {
   default = "deepak-putta.com"
 }
+
+variable "db_username" {
+  description = "username for RDS postgres instance"
+}
+
+variable "db_password" {
+  description = "password for RDS postgres instance"
+}
